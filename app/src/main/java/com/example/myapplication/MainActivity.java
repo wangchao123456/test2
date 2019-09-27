68b2737937a9ca2b445ed51db5aa43a150681871
wangchao <332134823@qq.com>
2019-09-27 15:37:21 +0800

    init

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //cat ~/.ssh/id_rsa.pub
+        //baidu/iov-dueros/auto-standard
+        //http://icode.baidu.com/repos/baidu/iov-dueros/auto-standard/tree/master/baidu/iov-dueros/auto-standard
     }
 }
